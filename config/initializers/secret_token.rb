99af19c9757626c4bf57621374cbe40545f757b6
alonso523 <alonso523@outlook.com>
2012-11-14 18:58:48 -0600

    Inicio tarea programada 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP4Alonso::Application.config.secret_token = 'd0e61e950989a3be699d0bc200d19687640041928f49491c4908618ab3aac1c269ee165847220dbc5add7e65e1da2b06bbd4b26f332c137aa925475e679dfa62'
